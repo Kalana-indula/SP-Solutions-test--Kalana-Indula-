3b67c0fb3d13993db288e2e46b15942477289b6d
Kalana-indula <kalanasmedia@outlook.com>
2025-12-05 13:10:27 +0530

    layout page added

--- NEWS Website/front-end/app/(root)/layout.tsx
+++ NEWS Website/front-end/app/(root)/layout.tsx
@@ -1,10 +1,10 @@
-// app/news/layout.tsx
+
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
-            {/* Section-level layout (e.g. sidebar, header, etc.) */}
+
             {children}
         </div>
     );
